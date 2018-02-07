92b4ca9667b2becb2decefacb32980915ec49172
Pommanthebrit <warburmf@hotmail.com>
2018-02-07 12:12:06 +0800

    Initialised
    the beginning of the documnets

---
+++ Get Back Alien Scum! ChrisAlpha/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	#region Enemy Variables
+	public GameObject[] myEnemy;
+	float minSpawnRate, maxSpawnRate;
+	#endregion
+
+	void Start () {
+		minSpawnRate = 0.8f;
+		maxSpawnRate = 1.6f;
+		InvokeRepeating ("SpawnEnemy", 6f, Random.Range(minSpawnRate, maxSpawnRate));
+	}
+	
+	void Update () {
+		
+	}
+
+	void SpawnEnemy () {
+		Vector2 minMoveLimit = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0.05f));
+		Vector2 maxMoveLimit = Camera.main.ViewportToWorldPoint (new Vector2 (0.95f, 1));
+
+		GameObject spawnEnemy = (GameObject)Instantiate (myEnemy [Random.Range (0, myEnemy.Length)]);
+		spawnEnemy.transform.position = new Vector2 (Random.Range (minMoveLimit.x, maxMoveLimit.x), maxMoveLimit.y);
+	}
+}
