8fcd39bec9212151a707eb974df8547a2e499ae4
CrimsonFury82 <1007626@student.sae.edu.au>
2018-02-13 14:29:38 +0800

    Astons changes

--- Get Back Alien Scum! ChrisAlpha/Assets/Scripts/Shoot.cs
+++ Get Back Alien Scum! ChrisAlpha/Assets/Scripts/Shoot.cs
@@ -4,21 +4,25 @@ using UnityEngine.EventSystems;
 
 public class Shoot : MonoBehaviour {
 
-	public GameObject projectile;
+	public GameObject projectile, LeftBarrel, RightBarrel;
 	public float bulletSpeed;
 
 	void Update () {
 
-		if (Input.touchCount > 0) {
-
-			if (Input.GetTouch (0).phase == TouchPhase.Began) {
-
-				Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-				Vector2 dir = touchPos - (new Vector2(transform.position.x, transform.position.y));
+//			if (Input.GetTouch (0).phase == TouchPhase.Began) {
+			if (Input.GetButtonDown("Fire1")) {
+//				Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+//				Vector2 dir = touchPos - (new Vector2(transform.position.x, transform.position.y));
+				Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);	
+				transform.rotation = Quaternion.LookRotation(Vector3.forward, worldMousePos- transform.position);
+				Vector2 dir = (Vector2)((worldMousePos - transform.position));
 				dir.Normalize ();
-				GameObject bullet = Instantiate (projectile, transform.position, Quaternion.identity)as GameObject;
+				GameObject bullet = Instantiate (projectile, LeftBarrel.transform.position, Quaternion.identity)as GameObject;
+				GameObject bullet2 = Instantiate (projectile, RightBarrel.transform.position, Quaternion.identity)as GameObject;
 				bullet.GetComponent<Rigidbody2D> ().velocity = dir * bulletSpeed; 
+				bullet.transform.rotation = Quaternion.LookRotation(Vector3.forward, worldMousePos- transform.position);
+				bullet2.GetComponent<Rigidbody2D> ().velocity = dir * bulletSpeed; 
+				bullet2.transform.rotation = Quaternion.LookRotation(Vector3.forward, worldMousePos- transform.position);
 			}			
-		}		
 	}
 }
