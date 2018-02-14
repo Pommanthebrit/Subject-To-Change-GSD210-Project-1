110d2bd2afa6e00d16978d78452c50eb2688e729
kcrash201 <10089172@student.sae.edu.au>
2018-02-14 14:04:56 +0800

    Added Pew SFX and Flashing script
    Flashing script needs collider

--- Get Back Alien Scum! ChrisAlpha/Assets/Scripts/Player.cs
+++ Get Back Alien Scum! ChrisAlpha/Assets/Scripts/Player.cs
@@ -25,4 +25,14 @@ public class Player : MonoBehaviour {
 			Debug.Log ("Score " + myGameController.myScore);
 		}
 	}
-}
+		void Damage() {
+			StartCoroutine(DamageBlink()); //calls coroutine DamageBlink
+			//other damage code here
+		}
+
+		IEnumerator DamageBlink() { //coroutine for making the player ship flash when taking damage
+			GetComponent<SpriteRenderer> ().color = new Color (1, 0, 0); //changes ship colour to red
+			yield return new WaitForSeconds (0.1f); //waits 0.1 seconds
+			GetComponent<SpriteRenderer> ().color = new Color (1, 1, 1); //changes ship back to previous colour
+		}
+	}
