7110de0312f23b94e49a458632d5f36639b2ec2f
kcrash201 <10089172@student.sae.edu.au>
2018-02-14 14:25:49 +0800

    Added Turret Animation
    Both cannons fire at same time

--- Get Back Alien Scum! ChrisAlpha/Assets/Scripts/Shoot.cs
+++ Get Back Alien Scum! ChrisAlpha/Assets/Scripts/Shoot.cs
@@ -7,26 +7,28 @@ public class Shoot : MonoBehaviour {
 	public GameObject myProjectile, LeftBarrel, RightBarrel;
 	public float bulletSpeed;
 
+
+
 		void Update () {
 
 //			if (Input.GetTouch (0).phase == TouchPhase.Began) {
-			if (Input.GetButtonDown("Fire1")) {
+		if (Input.GetButtonDown ("Fire1")) {
 //				Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
 //				Vector2 dir = touchPos - (new Vector2(transform.position.x, transform.position.y));
 
-				Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);	
-				transform.rotation = Quaternion.LookRotation(Vector3.forward, worldMousePos- transform.position);
-				Vector2 dir = (Vector2)((worldMousePos - transform.position));
-				dir.Normalize ();
+			Vector3 worldMousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);	
+			transform.rotation = Quaternion.LookRotation (Vector3.forward, worldMousePos - transform.position);
+			Vector2 dir = (Vector2)((worldMousePos - transform.position));
+			dir.Normalize ();
 
-				GameObject bullet = Instantiate (myProjectile, LeftBarrel.transform.position, Quaternion.identity)as GameObject;
-				GameObject bullet2 = Instantiate (myProjectile, RightBarrel.transform.position, Quaternion.identity)as GameObject;
+			GameObject bullet = Instantiate (myProjectile, LeftBarrel.transform.position, Quaternion.identity)as GameObject;
+			GameObject bullet2 = Instantiate (myProjectile, RightBarrel.transform.position, Quaternion.identity)as GameObject;
 
-				bullet.GetComponent<Rigidbody2D> ().velocity = dir * bulletSpeed; 
-				bullet.transform.rotation = Quaternion.LookRotation(Vector3.forward, worldMousePos- transform.position);
+			bullet.GetComponent<Rigidbody2D> ().velocity = dir * bulletSpeed; 
+			bullet.transform.rotation = Quaternion.LookRotation (Vector3.forward, worldMousePos - transform.position);
 
-				bullet2.GetComponent<Rigidbody2D> ().velocity = dir * bulletSpeed; 
-				bullet2.transform.rotation = Quaternion.LookRotation(Vector3.forward, worldMousePos- transform.position);
-			}			
+			bullet2.GetComponent<Rigidbody2D> ().velocity = dir * bulletSpeed; 
+			bullet2.transform.rotation = Quaternion.LookRotation (Vector3.forward, worldMousePos - transform.position);
+		}
 	}
 }
