bac285a88ac01483dbd2cd36a7cf449167198969
CrimsonFury82 <1007626@student.sae.edu.au>
2018-02-20 10:35:53 +0800

    Shoot updated

--- Get Back Alien Scum! ChrisAlpha/Assets/Scripts/Player/Shoot.cs
+++ Get Back Alien Scum! ChrisAlpha/Assets/Scripts/Player/Shoot.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using UnityEngine.EventSystems;
 
+//script written by Aston Olsen
+
 public class Shoot : MonoBehaviour {
 
 	Animator myAnimator;
@@ -17,28 +19,33 @@ public class Shoot : MonoBehaviour {
 	}
 
 	void Update () {
-
-//		if (Input.GetTouch (0).phase == TouchPhase.Began) {
+		//if (Input.GetTouch (0).phase == TouchPhase.Began) {
 		if (Input.GetButtonDown ("Fire1")) {
-//			Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-//			Vector2 dir = touchPos - (new Vector2(transform.position.x, transform.position.y));
 
 			myAnimator.SetTrigger ("Shoot");
 			myAudioSource.PlayOneShot (shoot[Random.Range (0, shoot.Length)]);
 
 			Vector3 worldMousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);	
-			transform.rotation = Quaternion.LookRotation (Vector3.forward, worldMousePos - transform.position);
-			Vector2 dir = (Vector2)((worldMousePos - transform.position));
-			dir.Normalize ();
+			//Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
 
+			transform.rotation = Quaternion.LookRotation (Vector3.forward, worldMousePos - transform.position);
+			//transform.rotation = Quaternion.LookRotation (Vector3.forward, touchPos - new Vector2(transform.position.x,transform.position.y));
+
+			Vector2 dir = (Vector2)((worldMousePos - transform.position));
+			//Vector2 dir = touchPos - (new Vector2(transform.position.x, transform.position.y));
+
+			dir.Normalize ();
 			GameObject bullet = Instantiate (myProjectile, LeftBarrel.transform.position, Quaternion.identity)as GameObject;
 			GameObject bullet2 = Instantiate (myProjectile, RightBarrel.transform.position, Quaternion.identity)as GameObject;
-
 			bullet.GetComponent<Rigidbody2D> ().velocity = dir * bulletSpeed; 
+
 			bullet.transform.rotation = Quaternion.LookRotation (Vector3.forward, worldMousePos - transform.position);
+			//bullet.transform.rotation = Quaternion.LookRotation (Vector3.forward, touchPos - new Vector2(transform.position.x,transform.position.y));
 
 			bullet2.GetComponent<Rigidbody2D> ().velocity = dir * bulletSpeed; 
+
 			bullet2.transform.rotation = Quaternion.LookRotation (Vector3.forward, worldMousePos - transform.position);
+			//bullet2.transform.rotation = Quaternion.LookRotation (Vector3.forward, touchPos - new Vector2(transform.position.x,transform.position.y));
 		}
 	}
 }
