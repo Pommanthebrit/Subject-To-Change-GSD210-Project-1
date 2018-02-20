a43636653908738a7b3674a788bb9eac9ed7f9b1
Geurtsy <geurtsy.gaming@gmail.coim>
2018-02-20 12:30:57 +0800

    Optimised enemies using EnemyController. Added audio functionality to EnemyController. Player now takes damage upon reaching beyond the boundaries of the screen space.

--- Get Back Alien Scum! ChrisAlpha/Assets/Scripts/Player/Player.cs
+++ Get Back Alien Scum! ChrisAlpha/Assets/Scripts/Player/Player.cs
@@ -15,23 +15,26 @@ public class Player : MonoBehaviour {
 	}
 
 	//Collision detection for player turret where applicable
-	void OnCollisionEnter2D(Collision2D collider) {
+	void OnCollisionEnter2D(Collision2D other) {
 		//Debug.Log ("Player hit by " + collider.gameObject.name);
 
 		if (collider.gameObject.tag == "Enemy") {
-
-			StartCoroutine(DamageBlink());
-
-			myGameController.myHealth += -1;
-			Debug.Log ("Player health " + myGameController.myHealth);
-			Destroy (collider.gameObject);
-			Debug.Log ("Score " + myGameController.myScore);
+			TakeDamage ();
+			Destroy (other.gameObject);
 		}
 	}
 
-		IEnumerator DamageBlink() { //coroutine for making the player ship flash when taking damage
-			GetComponent<SpriteRenderer> ().color = new Color (1, 0, 0); //changes ship colour to red
-			yield return new WaitForSeconds (0.1f);
-			GetComponent<SpriteRenderer> ().color = new Color (1, 1, 1); //changes ship back to previous colour
-		}
+	public void TakeDamage() {
+		StartCoroutine(DamageBlink());
+
+		myGameController.myHealth += -1;
+		Debug.Log ("Player health " + myGameController.myHealth);
+		Debug.Log ("Score " + myGameController.myScore);
+	}
+
+	IEnumerator DamageBlink() { //coroutine for making the player ship flash when taking damage
+		GetComponent<SpriteRenderer> ().color = new Color (1, 0, 0); //changes ship colour to red
+		yield return new WaitForSeconds (0.1f);
+		GetComponent<SpriteRenderer> ().color = new Color (1, 1, 1); //changes ship back to previous colour
+	}
 }
